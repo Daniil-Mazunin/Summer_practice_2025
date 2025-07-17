7e44b42e8ecf9d0693d5336870c9b69fc8baa7ba
Daniil-Mazunin <dsss6872@gmail.com>
2025-07-18 00:33:18 +0600

    feat: added TestCommand

---
+++ task19/TestCommand.cs
@@ -0,0 +1,29 @@
+﻿using task18;
+using CommandLib;
+
+namespace task19;
+
+public class TestCommand : ICommand
+{
+    public int Counter = 0;
+    private int Id;
+    private int MaxCount;
+    private IScheduler Scheduler;
+    public TestCommand(IScheduler scheduler, int id, int maxCount)
+    {
+        Scheduler = scheduler;
+        Id = id;
+        MaxCount = maxCount;
+    }
+    public void Execute()
+    {
+        if (Counter < MaxCount)
+        {
+            Console.WriteLine($"Поток {Id} вызов {++Counter}");
+            if (Counter < MaxCount)
+            {
+                Scheduler.Add(this);
+            }
+        }
+    }
+}
